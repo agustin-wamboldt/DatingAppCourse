19d5d305505537ce0b06df925e10753bd2c99100
AgustinW <aguswamboldt@gmail.com>
2022-04-06 11:28:19 -0300

    Added Toastr for notifications. Added Angular Guard routes to stop non logged in users from accessing certain functionality.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,10 @@ import { MessagesComponent } from './messages/messages.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot() // forRoot: has services or components that it needs to initialize along with the root module
+    BsDropdownModule.forRoot(), // forRoot: has services or components that it needs to initialize along with the root module
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
