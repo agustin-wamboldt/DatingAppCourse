f99b891b23db5f6b2b0003b7fb587d966b7cb9d6
AgustinW <aguswamboldt@gmail.com>
2022-05-26 09:54:56 -0300

    Added an action filter to update last activity for user in API.

---
+++ API/Helpers/LogUserActivity.cs
@@ -0,0 +1,27 @@
+﻿using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            // We want to wait until the User has already finished doing whatever they are doing to fire up this filter.
+            // Thus, the context we will work with (resultContext) will be the one given after 'next' is finished.
+            var resultContext = await next();
+
+            if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
+
+            var username = resultContext.HttpContext.User.GetUsername();
+            var repo = resultContext.HttpContext.RequestServices.GetService<IUserRepository>(); // Service Locator Pattern 
+            var user = await repo.GetUserByUsernameAsync(username); // Not performant since it loads photos too.
+            user.LastActive = DateTime.Now;
+            await repo.SaveAllAsync();
+        }
+    }
+}
