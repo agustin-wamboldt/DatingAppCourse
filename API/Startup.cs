fe8e8b8b893535a406f2227865e651f6f72d746c
AgustinW <aguswamboldt@gmail.com>
2022-06-13 09:38:09 -0300

    Preparing app for publishing: built client app, removed direct links to urls in client app, removed swagger (since this app will be deployed in dev mode) and attached client app to API.

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,12 +33,12 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                //app.UseDeveloperExceptionPage();  // Removed since we'll be using our custom middleware for handling expections.
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    //app.UseDeveloperExceptionPage();  // Removed since we'll be using our custom middleware for handling expections.
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            //}
 
             app.UseMiddleware<ExceptionMiddleware>();
 
@@ -51,6 +51,9 @@ namespace API
 
             app.UseAuthentication(); // Order matters, UseAuthentication first, UseAuthorization second
             app.UseAuthorization();
+
+            app.UseDefaultFiles(); // If index.html exists, use it and serve it
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
