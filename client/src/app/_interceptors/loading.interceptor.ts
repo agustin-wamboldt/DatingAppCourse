6bae13bd15716ff7d80dd37dfe60e37f73d501bf
AgustinW <aguswamboldt@gmail.com>
2022-06-13 09:51:10 -0300

    Removed the fake delay from the loading interceptor.

--- client/src/app/_interceptors/loading.interceptor.ts
+++ client/src/app/_interceptors/loading.interceptor.ts
@@ -17,7 +17,6 @@ export class LoadingInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.busyService.busy();
     return next.handle(request).pipe(
-      delay(1000), // TODO: Remove this before deploying, since it's fake delay to test this functionality.
       finalize(() => {
         this.busyService.idle();
       })
